d88411d7e30763c44324859c419f738077341b92
yanan <yananzx@yeah.net>
2019-08-12 23:53:14 +0800

    fix bug

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,13 +2,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		int a;
-		if( 19 > 27 || !(1 > 0 && 1 > 9 || 2 > 1) ){
-			a = 1;
-		}else{
-			a = 0;
-		}
-		System.out.println(a);
-
 		if( 19 > 27 || !(1 > 0 && 1 > 9 && 2 > 1) ){
 			a = 1;
 		}else{
